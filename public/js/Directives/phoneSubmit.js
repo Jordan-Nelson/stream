73ea4e3367e50c1382c2495c9cacf295ba79a3e8
Jordan <Jordnelson@deloitte.com>
2016-12-03 21:26:25 -0500

    hiding keyboard on iOS

--- public/js/Directives/phoneSubmit.js
+++ public/js/Directives/phoneSubmit.js
@@ -1,7 +1,7 @@
 var module = angular.module("Stream");
 module.directive('phoneSubmit', function () {
     return function (scope, element, attr) {
-        var textFields = $(element).children('div').children('input[type=text]');
+        var textFields = $(element).children('div').children('input[type=search]');
         $(element).submit(function() {
             textFields.blur();
         });
